657b75b5f30d333c8ef0a3d700c8c29d131439a3
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-09-01 17:42:46 +0200

    Update sketch_01.js

--- js/sketch_01.js
+++ js/sketch_01.js
@@ -26,7 +26,7 @@ function randomEmoji() {
   return EMOJIS[randIndex];
 }
 const NEW_LINE = '$'
-const SPOOKY_TEXT = 'HYPER SECRET STUDIO IS GOING UNDER RECONSTRUCTION.' + NEW_LINE + '🚧 🏗 👷'
+const SPOOKY_TEXT = 'HYPER SECRET STUDIO IS GOING UNDER CONSTRUCTION.' + NEW_LINE + '🚧 🏗 👷'
 
 
 let index = 0;
